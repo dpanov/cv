dca200f46fc28bf3fcecc2d596cd2d6e559a99e6
Dimitar Panov <mail@dpanov.net>
2020-02-13 17:41:46 +0200

    Added nav active state animation; Added transition properties in constants; Updated Readme.md

--- README.md
+++ README.md
@@ -3,18 +3,25 @@ Recreation of falcon.io main menu that is more accessible and has smoother trans
 
 ## Performance features:
  - No JS used for transitions
- - Only hardware-accelerated CSS property used for transitions (transform and opacity)
+ - Mosttly hardware-accelerated CSS property are used for transitions (transform and opacity). Exception are:
+    - Width of the active state element. It can be done with transform: scaleX(), but it will cause unnecessary code complications.
+    - Button/link colors
  - Less DOM elements
 
 ## Accessibility features:
  - Fully accessible by keyboard
+ - Fully accessible by screen readers
  - Proper :focus styling for all focusable elements
  - WCAG AAA compliant color contrast
  - `Skip to content` link
  - Semantic HTML (e.g. a button should really be a <button>, not a <p>)
- - Takes into consideration prefers-reduced-motion
+ - Takes into consideration `prefers-reduced-motion`
 
-## CSS architecture
- - Use of CSS variables for things like primary color and button dimensions means easier styling and future updates
+## CSS stuff
+ - Use of CSS variables for things like primary color and button dimensions means easier styling and future changes
  - Dark mode support
  - Button text is properly centered vertically
+
+## Things to consider
+ - dropdown menu slide animation is a bit different - in order to use only hardware-accelerated CSS properties the sliding logic of the dropdown menu is a bit different than the original one.
+ - Some colors are different due to WCAG compliance and also due to changing opacity level for some of them, instead of changing the background-color. Of course, both changes must be coordinated with the designer or the product owner in order to be in line with the brand identity.
--- src/js/menu.js
+++ src/js/menu.js
@@ -3,18 +3,24 @@ function menu() {
 
 	const Nav = document.querySelector('.js-nav');
 	const Header = document.querySelector('.js-header');
-	const NavTogglers = Nav.querySelectorAll('.js-nav-toggler');
 	const Body = document.querySelector('body');
+	
+	const NavTogglers = Nav.querySelectorAll('.js-nav-toggler');
+	const NavActiveState = document.querySelector('.js-nav-active-state');
 
 	initMenu();
 
 	function toggleMenu(clickedButton) {
 		clickedButton.addEventListener('click', () => {
+			setActiveState(clickedButton);
 
 			// Toggle dropdown
 			if (clickedButton.classList.contains(toggledClass)) {
+				// If you click the active button
 				NavTogglers.forEach(button => button.classList.remove(toggledClass));
+				NavActiveState.style.width = 0;
 			} else {
+				// If you click any of the buttons, but not the active one
 				NavTogglers.forEach(button => button.classList.remove(toggledClass));
 				clickedButton.classList.toggle(toggledClass);
 			}
@@ -28,6 +34,15 @@ function menu() {
 		})
 	}
 
+	// Set active state position
+	function setActiveState(clickedElement) {
+		const offsetLeft = clickedElement.getBoundingClientRect().left;
+		const width = clickedElement.getBoundingClientRect().width;
+
+		NavActiveState.style.transform = 'translateX(' + offsetLeft + 'px)';
+		NavActiveState.style.width = width + 'px';
+	}
+
 	function initMenu() {
 		// Ensure clicks in the header area don't close the nav dropdown
 		Header.addEventListener('click', (e) => {
@@ -39,6 +54,7 @@ function menu() {
 			if (Body.classList.contains(toggledClass)) {
 				Body.classList.remove(toggledClass);
 				NavTogglers.forEach(button => button.classList.remove(toggledClass));
+				NavActiveState.style.width = 0;
 			}
 		})
 		
@@ -46,6 +62,9 @@ function menu() {
 		NavTogglers.forEach((button) => {
 			toggleMenu(button);
 		});
+
+		// Init active state
+		NavActiveState.style.transform = 'translateX(' + Nav.getBoundingClientRect().left - 100 + 'px)';
 	}
 }
 
